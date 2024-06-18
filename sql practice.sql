3d83948fbe0d8dc9330288090c7fa37bd11e421a
manu <mahroofraj13@gmail.com>
2024-06-18 22:50:17 +0530

    practice in sql and more

---
+++ sql practice.sql
@@ -0,0 +1,51 @@
+use amazon;
+-- GROUP BY:
+SELECT location, SUM(stock) AS total_stock 
+FROM products 
+GROUP BY location;
+
+SELECT CASE 
+ WHEN price BETWEEN 0 AND 10000 THEN '0-10000' 
+ WHEN price BETWEEN 10001 AND 20000 THEN '10000-20000'
+ WHEN price BETWEEN 20001 AND 50000 THEN '20000-50000' 
+ ELSE '50000+' 
+ END AS price_range, COUNT(*) AS product_count
+FROM products 
+GROUP BY price_range;
+
+SELECT SUBSTRING(addr, 1, 3) AS location, AVG(age) AS avg_age 
+FROM customer 
+GROUP BY location;
+
+-- ORDER BY:
+SELECT * 
+FROM products 
+ORDER BY price DESC;
+
+SELECT * 
+FROM customer 
+ORDER BY age ASC;
+
+SELECT o.oid, c.cname, o.amt 
+FROM orders o 
+JOIN customer c ON o.cid = c.cid 
+ORDER BY o.amt DESC, c.cname ASC;
+
+-- HAVING:
+SELECT location, SUM(stock) AS total_stock 
+FROM products 
+GROUP BY location 
+HAVING SUM(stock) > 20;
+
+SELECT c.cid, c.cname, SUM(o.amt) AS total_amount 
+FROM customer c 
+JOIN orders o ON c.cid = o.cid 
+GROUP BY c.cid, c.cname 
+HAVING SUM(o.amt) > 10000;
+
+SELECT p.pid, p.pname, p.stock 
+FROM products p 
+WHERE p.location = 'Mumbai' 
+GROUP BY p.pid, p.pname, p.stock 
+HAVING p.stock BETWEEN 10 AND 20;
+ 
